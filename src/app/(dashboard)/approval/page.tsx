a6ba2875e5351d40075ecd831f337276f9d04163
Yakub shakirudeen Olaide <farmerintech@gmail.com>
2025-06-11 16:56:50 +0100

    Update page.tsx

--- src/app/(dashboard)/approval/page.tsx
+++ src/app/(dashboard)/approval/page.tsx
@@ -1,67 +1,25 @@
 "use client"
 
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table";
-  import { FaBan, FaEye, FaPencilAlt, FaTrash } from "react-icons/fa";
-  import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-import { useEffect, useState } from "react";
-import { useUser } from "../context/reducer";
-import Link from "next/link";
+import { ApprovalTable } from '@/app/components/approvalTable';
+import { DashHook } from '@/app/components/dahHook';
+import { UploadsTable } from '@/app/components/uploadsTable';
+import { UsersTable } from '@/app/components/usersTable';
+import { useUser } from '@/app/context/reducer'
+import { useSidebar } from '@/app/context/sideBarState';
+import React, { useEffect, useState } from 'react'
+import { FaArrowUp } from 'react-icons/fa';
 
-  const actions = [
-    {
-      name: "View",
-      icon: <FaEye size={15}/>,
-      color: "text-blue-400",
-    },
-    {
-      name: "Edit",
-      icon: <FaPencilAlt size={15}/>,
-      color: "text-yellow-400",
-    },
-    {
-      name: "Delete",
-      icon: <FaTrash size={15}/>,
-      color: "text-red-400",
-    },
-  ];
-
-
-  interface userInterface {
-    data: {
-      uploadType: string;
-      subject:string;
-      createdAt:any,
-      uploadedBy:string
-    }[];
-  }
-  
-  const filterItems = ["Subject", "Upload Type"]
-  export function ApprovalTable() {
-    const [data, setData] = useState<any>();
-    const [error, setError] = useState<string>()
-    const [message, setMessage] = useState('')
-const {state} = useUser()
-
+export default function page() {
+  const [data, setData] = useState<any>({})
+  const [page, setpage] = useState<any>({})
+  const [error, setError]  = useState<string>()
+    const { sidebarOpen, setSidebarOpen } = useSidebar();
+    const {state}=useUser()
  useEffect(() => {
       const fetchData = async () => {
         try {
           const response = await fetch(
-            "https://citadel-i-project.onrender.com/api/v1/uploads/all",
+            "https://citadel-i-project.onrender.com/api/v1/uploads",
             {
               method: "GET",
               credentials: 'include',
@@ -73,9 +31,9 @@ const {state} = useUser()
           );
   
           const result = await response.json();
-          setData(result.data);
+          setData(result.pagination);
   
-          console.log(result);
+          console.log(data);
   
           !response.ok && setError(result?.message || "Something went wrong");
         } catch (error) {
@@ -85,128 +43,74 @@ const {state} = useUser()
       };
       fetchData();
     }, [data]);
-    const [filter, setFilter] = useState('')
-    const handleSelectFilter = (value:string) =>{
-      setFilter(value)
-    }
-const filterBy = async ()=>{
-  try {
-    const response = await fetch(
-      `https://citadel-i-project.onrender.com/api/v1/uploads`,
-      {
-        method: "GET",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // body: JSON.stringify(''),
-      }
-    );
 
-    const result = await response.json();
-    setData(result.data);
-
-    console.log(result);
-
-    !response.ok && setError(result?.message || "Something went wrong");
-  } catch (error) {
-    console.error(error);
-    setError("Error connecting to server");
-  }
-};
-
-const Approve = async (Type:string, id:number)=>{
-  try {
-      console.log(Type, id)
-
-    const response = await fetch(
-      `https://citadel-i-project.onrender.com/api/v1/uploads/approve/${id}`,
-      {
-        method: "POST",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({uploadType:Type}),
-      }
-    );
-
-    const result = await response.json();
-    setMessage(result.message);
-    console.log(result);
-
-    !response.ok && setError(result?.message || "Something went wrong");
-  } catch (error) {
-    console.error(error);
-    setMessage('')
-    setError("Error connecting to server");
-  }
-};
-
-const Disapprove = async (Type:string, id:number)=>{
-  console.log(Type, id)
-  try {
-    const response = await fetch(
-      `https://citadel-i-project.onrender.com/api/v1/uploads/disapprove/${id}`,
-      {
-        method: "POST",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({uploadType:Type}),
-      }
-    );
-
-    const result = await response.json();
-    setMessage(result.message);
-
-    console.log(result);
-
-    !response.ok && setError(result?.message || "Something went wrong");
-  } catch (error) {
-    console.error(error);
-    setError("Error connecting to server");
-  }
-};
-
-    return (
-    <section className="bg-white p-[16px] rounded-[8px]">
-        <p>{message && message}</p>
-    <form className="py-[8px] flex justify-between items-center flex-col md:flex-row gap-[8px] " onSubmit={filterBy}>
-      <p className="font-[600]">Recent Uploads</p>  
-    <button className="outline-none border-none bg-orange-500 px-[16px] py-[8px] text-white rounded-[8px]">View all</button>
-    </form>
-    <section className="rounded-md overflow-x-auto w-full px-1 mt-5">
-  <table className="min-w-full divide-y divide-gray-200 text-left text-sm">
-    <thead className="bg-gray-100">
-      <tr>
-        <th className="px-4 py-2 font-semibold text-gray-700">Subject</th>
-        <th className="px-4 py-2 font-semibold text-gray-700">Upload Type</th>
-        <th className="px-4 py-2 font-semibold text-gray-700">Uploaded By</th>
-        <th className="px-4 py-2 font-semibold text-gray-700">Status</th>
-        <th className="px-4 py-2 font-semibold text-gray-700">Perform Actions</th>
-      </tr>
-    </thead>
-<tbody className="divide-y divide-gray-100">
-  {data && data.map((item:any, index:number) => (
-    <tr key={index}>
-      <td className="px-4 py-2">{item.subject}</td>
-      <td className="px-4 py-2 text-xs md:text-sm">{item.uploadType}</td>
-      <td className="px-4 py-2">{item.uploadedBy}</td>
-      <td className={`px-4 py-2 ${item.status === "Pending" ? "text-yellow-500": item.status === "Approved" ?"text-green-500":"text-red-500"}`}>{item.status}</td>
-      <td className="px-4 py-2">
-        <div className="flex items-center gap-2">
-         <button onClick={()=>Approve(item.uploadType, item.id)} className="bg-green-500 text-white p-[4px] rounded-[4px] text-[12px]">Approve</button>
-         <button onClick={()=>Disapprove(item.uploadType, item.id)} className="bg-red-500 text-white p-[4px] rounded-[4px] text-[12px]">Disapprove</button>
+  const filterItems= ['By Teachers', 'By Students']
+  const percentageApr = Math.ceil((data?.totalApprovedCount/(data?.totalCount))*100) 
+  const percentageDis = Math.ceil((data?.totalDisapprovedCount/(data?.totalCount))*100)
+    return(
+      <section className={`w-ful px-[16px] pb-[24px] mt-6 min-h-screen ${sidebarOpen && "hidden md:block"}`}>
+      <DashHook name={"Dashboard Overview"}/>
+    <main className='grid xl:grid-cols-4 md:grid-cols-2 mt-10 gap-[16px]'>
+      <div className='bg-[#FFFFFF] p-[16px] gap-[10px] rounded-[8px] flex items-start justify-between'>
+        <div className='flex flex-col gap-[16px]'>
+          <p>Total Uploaded</p>
+          <div className='flex items-center gap-[8px]'>
+              <p className='text-[#0F0F0F] text-[24px] font-[700]'>{data && data?.totalCount}</p> 
+              <div className='flex text-[#0DAF64] text-[12px] '>
+                 <FaArrowUp/>
+              </div>
+            </div>
         </div>
-      </td>
-    </tr>
-  ))}
-</tbody>
-  </table>
-</section>
-</section>
-    );
-  }
-  
+        <div>
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="40" height="40" rx="20" fill="#FFEEE6"/>
+          <path d="M26.0001 23.8369C27.456 24.5683 28.7042 25.742 29.6153 27.2096C29.7957 27.5003 29.886 27.6456 29.9172 27.8468C29.9805 28.2558 29.7009 28.7585 29.32 28.9204C29.1326 29 28.9218 29 28.5001 29M24.0001 19.5322C25.4818 18.7959 26.5001 17.2669 26.5001 15.5C26.5001 13.7331 25.4818 12.2041 24.0001 11.4678M22.0001 15.5C22.0001 17.9853 19.9854 20 17.5001 20C15.0148 20 13.0001 17.9853 13.0001 15.5C13.0001 13.0147 15.0148 11 17.5001 11C19.9854 11 22.0001 13.0147 22.0001 15.5ZM10.5593 26.9383C12.1536 24.5446 14.6695 23 17.5001 23C20.3307 23 22.8465 24.5446 24.4409 26.9383C24.7901 27.4628 24.9648 27.725 24.9446 28.0599C24.929 28.3207 24.758 28.64 24.5496 28.7976C24.282 29 23.9139 29 23.1777 29H11.8224C11.0863 29 10.7182 29 10.4505 28.7976C10.2421 28.64 10.0712 28.3207 10.0555 28.0599C10.0354 27.725 10.21 27.4628 10.5593 26.9383Z" stroke="#FF5900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+      </div>
+      </div>
+      
+      <div className='bg-[#FFFFFF] p-[16px] gap-[10px] rounded-[8px] flex items-start justify-between'>
+        <div className='flex flex-col gap-[16px]'>
+          <p>Total Approved</p>
+          <div className='flex items-center gap-[8px]'>
+              <p className='text-[#0F0F0F] text-[24px] font-[700]'>{data && data?.totalApprovedCount}</p> 
+              <div className='flex text-[#0DAF64] text-[12px] '>
+                 <FaArrowUp/>
+                 <p>{percentageApr}%</p>
+              </div>
+            </div>
+        </div>
+
+
+        <div>
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="40" height="40" rx="20" fill="#FFEEE6"/>
+          <path d="M26.0001 23.8369C27.456 24.5683 28.7042 25.742 29.6153 27.2096C29.7957 27.5003 29.886 27.6456 29.9172 27.8468C29.9805 28.2558 29.7009 28.7585 29.32 28.9204C29.1326 29 28.9218 29 28.5001 29M24.0001 19.5322C25.4818 18.7959 26.5001 17.2669 26.5001 15.5C26.5001 13.7331 25.4818 12.2041 24.0001 11.4678M22.0001 15.5C22.0001 17.9853 19.9854 20 17.5001 20C15.0148 20 13.0001 17.9853 13.0001 15.5C13.0001 13.0147 15.0148 11 17.5001 11C19.9854 11 22.0001 13.0147 22.0001 15.5ZM10.5593 26.9383C12.1536 24.5446 14.6695 23 17.5001 23C20.3307 23 22.8465 24.5446 24.4409 26.9383C24.7901 27.4628 24.9648 27.725 24.9446 28.0599C24.929 28.3207 24.758 28.64 24.5496 28.7976C24.282 29 23.9139 29 23.1777 29H11.8224C11.0863 29 10.7182 29 10.4505 28.7976C10.2421 28.64 10.0712 28.3207 10.0555 28.0599C10.0354 27.725 10.21 27.4628 10.5593 26.9383Z" stroke="#FF5900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+      </div>
+      </div>
+    <div className='bg-[#FFFFFF] p-[16px] gap-[10px] rounded-[8px] flex items-start justify-between'>
+        <div className='flex flex-col gap-[16px]'>
+          <p>Total Disapproved</p>
+          <div className='flex items-center gap-[8px]'>
+              <p className='text-[#0F0F0F] text-[24px] font-[700]'>{data && data?.totalDisapprovedCount}</p> 
+              <div className='flex text-[#0DAF64] text-[12px] '>
+                 <FaArrowUp/>
+                 <p>{percentageDis}%</p>
+              </div>
+            </div>
+        </div>
+        <div>
+        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="40" height="40" rx="20" fill="#FFEEE6"/>
+        <path d="M14 19V23M26 17V21M25 12C27.4487 12 28.7731 12.3748 29.4321 12.6654C29.5199 12.7042 29.5638 12.7235 29.6904 12.8444C29.7663 12.9168 29.9049 13.1294 29.9405 13.2281C30 13.3927 30 13.4827 30 13.6627V24.4111C30 25.3199 30 25.7743 29.8637 26.0079C29.7251 26.2454 29.5914 26.3559 29.3319 26.4472C29.0769 26.5369 28.562 26.438 27.5322 26.2401C26.8114 26.1017 25.9565 26 25 26C22 26 19 28 15 28C12.5513 28 11.2269 27.6252 10.5679 27.3346C10.4801 27.2958 10.4362 27.2765 10.3096 27.1556C10.2337 27.0832 10.0951 26.8706 10.0595 26.7719C10 26.6073 10 26.5173 10 26.3373L10 15.5889C10 14.6801 10 14.2257 10.1363 13.9921C10.2749 13.7546 10.4086 13.6441 10.6681 13.5528C10.9231 13.4631 11.438 13.562 12.4678 13.7598C13.1886 13.8983 14.0435 14 15 14C18 14 21 12 25 12ZM22.5 20C22.5 21.3807 21.3807 22.5 20 22.5C18.6193 22.5 17.5 21.3807 17.5 20C17.5 18.6193 18.6193 17.5 20 17.5C21.3807 17.5 22.5 18.6193 22.5 20Z" stroke="#FF5900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </div>
+      </div>
+    </main>
+     <section className="py-[8px] flex flex-col gap-[8px] bg-white mt-20 rounded-[8px] px-[16px]">
+      <ApprovalTable/>
+    </section>
+    </section>
+  )
+}
