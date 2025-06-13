52196fca8c4005088f06d6e57dd1c50687694d5e
Yakub shakirudeen Olaide <farmerintech@gmail.com>
2025-06-13 18:16:04 +0100

    Update editPastQuestion.tsx

--- src/app/components/editPastQuestion.tsx
+++ src/app/components/editPastQuestion.tsx
@@ -157,7 +157,7 @@ useEffect(() => {
   return (
     <form className="md:flex md:justify-between md:gap-[60px] flex-col md:flex-row w-full" onSubmit={handleSubmit}>
       <section className="w-full flex gap-[16px] flex-col">
-            <Modal message={message && message} error={"error && error"}/>
+             <Modal message={message || ""} error={error || ""}/>
 
         {/* Subject */}
         <div className="flex flex-col gap-[8px] w-full">
